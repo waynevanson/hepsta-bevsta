f17d69750b4f72388521e2c3e7e1fa2cb21367a5
Wayne Van Son <=>
2024-09-05 08:46:17 +1000

    add flakes

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+    inputs = {
+        flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+        flake-utils.url = "github:numtide/flake-utils";
+        fenix = {
+            url = "github:nix-community/fenix";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { self, nixpkgs, flake-utils}:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = nixpkgs.legacyPackages.${system};
+            in
+                {
+                    shell.default = pkgs.mkShell rec {};
+                }
+        );
+}
